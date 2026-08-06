06c875a97d6e649e4e16602d286a41738401d8bf
Sharafat Ali <funnysarkar100@gmail.com>
2026-08-06 20:47:02 +0500

    another some code

--- ListComprehension.py
+++ ListComprehension.py
@@ -6,12 +6,25 @@
 
 # map method , def conversion 
 
-names = ['shahid' , 'khan', 'ali' , 'rafay']
-age = [22,44,12,76]
-FinalDic = {}
-for i,k in zip(names, age):
-    print(i,k)
-    # FinalDic.update({i:k})
-    FinalDic[i]=k
+NewList = [324,5352,3424,656,9]
+def Newfunction(i):
+    if(i % 2 == 0):
+        return 'even'
+    else :
+        return 'odd'
+            
+UpdatedList =  list(map(Newfunction , NewList))
+print(UpdatedList)
+
+# names = ['shahid' , 'khan', 'ali' , 'rafay']
+# age = [22,44,12,76]
+# FinalDic = {}
+# for i,k in zip(names, age):
+#     print(i,k)
+#     # FinalDic.update({i:k})
+#     FinalDic[i]=k
     
-print(FinalDic)
+# print(FinalDic)
+
+# Andic = {x:y for x,y in zip(names,age)}
+# print(Andic)
