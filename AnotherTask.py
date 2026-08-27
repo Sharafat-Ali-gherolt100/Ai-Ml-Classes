ea1e3ad1b51d2838c4a8e29a0b34e16976a86359
Sharafat Ali <funnysarkar100@gmail.com>
2026-08-27 17:31:54 +0500

    another some files of python

---
+++ AnotherTask.py
@@ -0,0 +1,12 @@
+# import numpy as np
+
+Mornings = [
+    {"clouds" : "yes" , "wind" : "strong" , "rain" : "yes"},
+    {"clouds" : "yes" , "wind" : "strong" , "rain" : "yes"},
+    {"clouds" : "yes" , "wind" : "strong" , "rain" : "no"},
+    {"clouds" : "yes" , "wind" : "strong" , "rain" : "yes"},
+    {"clouds" : "yes" , "wind" : "strong" , "rain" : "no"}
+]
+
+# for x,y in Mornings.:
+    
